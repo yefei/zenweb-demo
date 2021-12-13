fae38567774d265a5eaf1657d5a2974361d7cfc8
FeiYe <316606233@qq.com>
2021-12-13 23:10:11 +0800

    grid

--- app/controller/index.js
+++ app/controller/index.js
@@ -8,7 +8,8 @@ router.get('/', async ctx => {
   ctx.success('Hello ZenWeb!');
 });
 
-const GRID = Symbol('Grid');
+const GRID = Symbol('GridColumn#grid');
+const ATTRS = Symbol('GridColumn#attrs');
 
 class GridColumn {
   /**
@@ -17,17 +18,24 @@ class GridColumn {
    */
   constructor(grid, prop) {
     this[GRID] = grid;
-    this.prop = prop;
+    this[ATTRS] = { prop };
+  }
+
+  get attrs() {
+    return this[ATTRS];
+  }
+
+  attr(key, value) {
+    this[ATTRS][key] = value;
+    return this;
   }
 
   label(label) {
-    this.label = label;
-    return this;
+    return this.attr('label', label);
   }
 
-  order() {
-    this.sortable = 'custom';
-    return this;
+  sortable() {
+    return this.attr('sortable', 'custom');
   }
 }
 
@@ -41,9 +49,8 @@ class Grid {
     this.limit = 10;
   }
 
-  column(prop, label) {
+  column(prop) {
     const column = new GridColumn(this, prop);
-    if (label) column.label(label);
     this.columns.push(column);
     return column;
   }
@@ -52,36 +59,51 @@ class Grid {
     this.limit = count;
   }
 
-  orderCheck() {
-    // 排序字段检查
-    for (let i of order) {
-      if (i.startsWith('-')) {
-        i = i.slice(1);
-      }
-      if (!allowOrders.includes(i)) {
-        ctx.fail(`不允许的排序字段: ${i}`);
+  /**
+   * 排序字段过滤检查
+   * @param {string[]} input
+   * @returns 
+   */
+  orderFilter(input) {
+    const order = [];
+    const allowOrders = this.columns.filter(i => i.attrs.sortable).map(i => i.attrs.prop);
+    for (const i of input) {
+      if (allowOrders.includes(i.startsWith('-') ? i.slice(1) : i)) {
+        order.push(i);
       }
     }
+    return order;
   }
 
   async fetch(opt) {
+    const columns = this.columns.map(i => i.attrs.prop);
+    // 排序
+    const order = opt.order ? this.orderFilter(opt.order) : [];
+    if (order.length) {
+      this.finder.order(...order);
+    }
+    // 分页并取得指定列
+    const limit = opt.limit || this.limit;
+    const offset = opt.offset || 0;
     const result = await this.finder.page({
-      limit: opt.limit || this.limit,
-      offset: opt.offset || 0,
-    });
+      limit,
+      offset,
+    }, ...columns);
     return {
-      columns: this.columns,
+      columns: this.columns.map(i => i.attrs),
       data: result.list,
       total: result.total,
+      limit,
+      offset,
     };
   }
 }
 
 router.get('/grid', async ctx => {
   const grid = new Grid(ctx.model.user.find());
-  grid.column('id', 'ID').order();
-  grid.column('name', '姓名');
-  grid.column('birthday', '生日');
-  grid.column('created_at', '创建日期').order();
-  ctx.success(await grid.fetch(ctx.helper.query({ limit: 'int', offset: 'int' })));
+  grid.column('id').label('ID').sortable();
+  grid.column('name').label('姓名');
+  grid.column('birthday').label('生日');
+  grid.column('created_at').label('创建日期').sortable();
+  ctx.success(await grid.fetch(ctx.helper.query({ limit: 'int', offset: 'int', order: 'trim[]' })));
 });
